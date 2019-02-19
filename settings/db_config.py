33ff498aa4a072af4d00c0816f2712a031875588
wangyang <wangyang2@zhuge.com>
2019-02-19 17:14:51 +0800

    '1'

--- settings/db_config.py
+++ settings/db_config.py
@@ -4,7 +4,7 @@ old_city = ['bj', 'bd', 'cc', 'cd', 'cq', 'cs', 'cz', 'dg', 'dl', 'fz', 'guilin'
             'qz', 'sh', 'sjz', 'su', 'sx', 'sy', 'sz', 'taiyuan', 'taizhou', 'tj', 'ts', 'weihai', 'wf', 'wh', 'wx',
             'xa', 'xaxq', 'xm', 'xz', 'yc', 'yinchuan', 'yt', 'yz', 'zh', 'zs', 'zz']
 
-Online_Mysql = {
+Mysql = {
     'dm': {
         "host": "mysql.zhugefang.com",
         "user": "dm_rw",
@@ -42,23 +42,23 @@ Online_Mysql = {
     }
 }
 
-Mysql = {
+Online_Mysql = {
     'dm': {
-        "host": "101.201.119.240",
-        "user": "afe-rw",
-        "password": "HMugq0Fjz3bK67tHdSFottW2ORwSKpcJ",
+        "host": "rm-2zed5a6vhd5qk06z5.mysql.rds.aliyuncs.com",
+        "user": "dm_rw",
+        "password": "CszwRk3breCsM5BCH0yDfHLorJM5QB5T",
         "port": 3306,
         "charset": "utf8",
     },
     'bad_mysql': {
-        "host": "rm-2ze7398d890nw9k12po.mysql.rds.aliyuncs.com",
+        "host": "rm-2ze7398d890nw9k12.mysql.rds.aliyuncs.com",
         "user": "data_r",
         "password": "BQ6Qr1*dIh%##bK3zg5p0M6x@Mkqs&hg",
         "port": 3306,
         "charset": "utf8",
     },
     'sell_mysql': {
-        "host": "rm-2zegcz5fcw1k13su1.mysql.rds.aliyuncs.com",
+        "host": "rr-2ze7fny2kea5431cy.mysql.rds.aliyuncs.com",
         "user": "data_r",
         "password": "BQ6Qr1*dIh%##bK3zg5p0M6x@Mkqs&hg",
         # "port": 9521,
@@ -78,7 +78,9 @@ Mysql = {
         # "port": 9532,
         "charset": "utf8",
     }
+
 }
+
 Mongo = {
     'dios': 'mongodb://zhuge:7UgfAWbUtTKeMVGrSLwsHjB9xGjumnck@dds-2ze2f6d07e237dd41500-pub.mongodb.rds.aliyuncs.com:3717/admin'
 }
